e98b3ddca5001694dd949aab10d753eaa7ab4e3d
scrappy961205 <david.liz961205@gmail.com>
2022-11-24 04:17:54 -0500

    inicio

---
+++ test/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Estudio Test",
+    "summary": """Estudio""",
+    "description": """Estudio""",
+    "author": "Lizander",
+    "category": "Uncategorized",
+    "version": "1.0",
+    "depends": [
+
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
